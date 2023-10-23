61939372e884571e353c84430c42423bb694e3ff
Lungelo Ntuli <s220213909@mandela.ac.za>
2023-10-23 11:01:57 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,15 +1,15 @@
 #include "lists.h"
 
 /**
- * Return: the number of nodes
-**/
-size_t print_listint(const listint_t *h)
+ * Return: number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
 {
  size_t num = 0;
 
  while (h)
  {
- printf("%d\n", h->n);
  num++;
  h = h->next;
  }
